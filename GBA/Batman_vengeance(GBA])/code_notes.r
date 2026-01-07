1a8e67d43971a6e7cc228e1d5498ffda651fe7e7
Srleo12 <carlosnatha2345@gmail.com>
2026-01-07 17:59:47 -0300

    test

--- GBA/Batman_vengeance(GBA])/code_notes.r
+++ GBA/Batman_vengeance(GBA])/code_notes.r
@@ -1,3 +1,11 @@
+0x00001c: [8-bit] Timer (milisegundos)
+
+0x00001d: [8-bit] Timer (segundos)
+
+0x00001e: [8-bit] Timer (minutos)
+
+0x00001f: [8-bit] Timer (horas)
+
 0x000020: [8-bit] Batcinto
 0x00 = Batarang
 0x01 = Smoke bomb
@@ -5,26 +13,45 @@
 0x03 = Health potion
 
 0x000021: [8-bit] Batarangs 
-0x00 = vazio até 0x09 = full
+0x00 = vazio até 0x09 = full 
 
 0x000022: [8-bit] Smoke bombs 
-0x00 = vazio até 0x09 = full
+0x00 = vazio até 0x09 = full 
 
 0x000024: [8-bit] Health potions 
-0x00 = vazio até 0x09 = full
+0x00 = vazio até 0x09 = full 
+
+0x000025: [8-bit] Icone da barra de vida (robin)
+0x00 = invisivel
+0x01 = visivel 
 
 0x000028: [8-bit] Health bar
-max = 0x0c
+max = 0x0c 
 
-0x000254: Iminigos ?
-0x00 = Sim
-0x01 = Não
+0x0000e5: [8-bit] Icone da barra de vida (robin)
+0x00 = invisivel
+0x01 = visivel 
+
+0x000210: [8-bit] Tela de game over
+0x01 = in game
+0x00 = game over 
+
+0x000218: [8-bit] Indicador musica
+0x00 = sem musica
+0x04 = som de escolha menu
+0x44 = musica de inicio
+0x16 = musica menu
+0x64 = musica de encerramento da fase
+0x22 = musica de game over
+0x24 = musica primeira fase
+0x1c = musica segunda fase
+0x3a = musica terceira fase 
 
 0x003d84: [8-bit] Game pause
 0x00 = in game
-0x01 = pause
+0x01 = pause 
 
-0x003d90: [8-bit] Menu selection
+0x003d90: [8-bit] Menu selection 
 
 0x003d9c: [8-bit] Localização game
 0x02 = Cacsine
@@ -33,7 +60,7 @@ max = 0x0c
 0x13 = New Game/ Password
 0x14 = Advanced mode/Password
 0x1f = in game
-0x22 = Boss
+0x22 = Boss 
 
 0x003da8: [8-bit] Levels games
 0x00 = Level 1
@@ -56,49 +83,54 @@ max = 0x0c
 0x11 = Level 18
 0x12 = Level 19
 0x13 = Level 20
-0x14 = Level 21
+0x14 = Level 21 
 
 0x003e12: [8-bit] controle - A
-0x01 = ativa
+0x01 = ativa 
 
 0x003e13: [8-bit] controle - B
-0x01 = ativa
+0x01 = ativa 
 
 0x003e14: [8-bit] controle - L
-0x01 = ativa
+0x01 = ativa 
 
 0x003e15: [8-bit] controle - R
-0x01 = ativa
+0x01 = ativa 
 
 0x003e16: [8-bit] controle - cima
-0x01 = ativa
+0x01 = ativa 
 
 0x003e17: [8-bit] controle - baixo
-0x01 = ativa
+0x01 = ativa 
 
 0x003e18: [8-bit] controle - esquerda
-0x01 = ativa
+0x01 = ativa 
 
 0x003e19: [8-bit] controle - direita
-0x01 = ativa
+0x01 = ativa 
 
 0x004328: [8-bit] Batcinto em boss figth
 0x00 = Batarang
 0x01 = Smoke bomb
 0x02 = Batcorda
-0x03 = Health potion
+0x03 = Health potion 
 
 0x004329: [8-bit] Batarangs em boss figth
-0x00 = vazio até 0x09 = full
+0x00 = vazio até 0x09 = full 
 
 0x00432a: [8-bit] Smoke bombs em boss figth
-0x00 = vazio até 0x09 = full
+0x00 = vazio até 0x09 = full 
 
 0x00432c: [8-bit] Health potions em boss figth
-0x00 = vazio até 0x09 = full
+0x00 = vazio até 0x09 = full 
 
 0x004331: [8-bit] Health bar em boss figth
-max = 0x0c
+max = 0x0c 
 
-0x008334: [8-bit] vida Level 1 (editavel)
-0x008343: [8-bit] vida Level 1 (não editavel)
+0x008328: [8-bit] vida Level 2 (editavel) 
+
+0x008334: [8-bit] vida Level 1 (editavel) 
+
+0x008337: [8-bit] vida Level 2 (não editavel) 
+
+0x008343: [8-bit] vida Level 1 (não editavel) 
--- img_64x64.py
+++ img_64x64.py
@@ -1,4 +1,4 @@
 from PIL import Image
-imagem_original = Image.open("bagde1.png")
+imagem_original = Image.open("bagde.png")
 imagem_redimensionada = imagem_original.resize((96, 96), Image.LANCZOS)
-imagem_redimensionada.save("bagde.png")
+imagem_redimensionada.save("bagde1.png")
